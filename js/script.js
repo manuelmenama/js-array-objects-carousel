1d6357f4fd3ef7b0fb978811fc6a059c70c5dd37
manuelmenama <manuel.mena090589@gmail.com>
2022-10-26 16:36:16 +0200

    added html - css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('Ciao world!');
+console.log('New Carousel loaded');
+
+/*
+1 - definisco il contenuto di un array di oggetti
+2 - stampo le immagini dell'album
+3 - stampo le immagini del navigatore
+4 - funzioni per il controllo delle frecce del carosello
+5 - alla pressione del tasto start
+    - nascono il tasto start
+    - mostro il tasto stop
+6 - alla pressione del tasto reverse
+    - cambia la direzione del carosello
+
+
+*/
